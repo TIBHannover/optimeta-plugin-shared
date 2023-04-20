03fb2b704ec6d107fd1fe901a3667e58a32ddbb8
GaziYucel <GaziYucel@users.noreply.github.com>
2023-04-20 14:57:17 +0200

    OpenAlexBase

--- src/OpenAlex/OpenAlexBase.php
+++ src/OpenAlex/OpenAlexBase.php
@@ -9,40 +9,58 @@ use Optimeta\Shared\OpenAlex\Model\Work;
 class OpenAlexBase
 {
     /**
+     * User agent name to identify us
+     * @var string
+     */
+    protected string $userAgent = 'OJSOptimetaCitations';
+
+    /**
      * @desc The url to the api
      * @var string
      */
-    protected $baseUrl = 'https://api.openalex.org/';
+    protected string $url = 'https://api.openalex.org/';
+
+    /**
+     * @desc GuzzleHttp\Client
+     * @var object (class)
+     */
+    protected object $client;
 
     public function __construct()
     {
+        if (!empty(OPTIMETA_CITATIONS_USER_AGENT))
+            $this->userAgent = OPTIMETA_CITATIONS_USER_AGENT;
 
+        $this->client = new Client([
+            'headers' => ['User-Agent' => $this->userAgent],
+            'verify' => false
+        ]);
     }
 
     /**
+     * Get Work from OpenAlex Api
      * @param $doi
-     * @return object (Work)
-     * @throws GuzzleException
+     * @return Work
      */
-    public function getWorkFromApiAsObjectWithDoi($doi): object
+    public function getWorkFromApiAsObjectWithDoi($doi): Work
     {
         $work = new Work();
 
         if (empty($doi)) return $work;
 
-        try{
-            $response = $this->client->request('GET', $this->baseUrl . 'works/doi:' . $doi);
+        try {
+            $response = $this->client->request('GET', $this->url . 'works/doi:' . $doi);
             $responseBody = $response->getBody();
             $responseBodyArray = json_decode($responseBody, true);
 
-
-            foreach($responseBodyArray as $key => $value){
-                if(property_exists($work, $key)){
+            foreach ($responseBodyArray as $key => $value) {
+                if (property_exists($work, $key)) {
                     $work->$key = $value;
                 }
             }
+        } catch (GuzzleException|\Exception $ex) {
+            error_log($ex->getMessage(), true);
         }
-        catch(\Exception $ex){}
 
         return $work;
     }
